093175aac03f7555bb2cc0074775937ca63cee7b
Jan Redmann <janredmann1@gmail.com>
2022-08-18 10:56:27 +0200

    add image

---
+++ p5-intro/intro.js
@@ -0,0 +1,14 @@
+// this preloads all the assets (images, sounds, etc)
+function preload() {
+	console.log('preload')
+}
+// this is used to setup the html canvas
+function setup() {
+	console.log('setup')
+	createCanvas(1000, 600)
+}
+
+// this is the main draw function
+function draw() {
+
+}
